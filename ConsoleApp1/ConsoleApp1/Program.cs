52ee877d973f62e6c30ee8a347450a5e6d66a535
tabbbyzzxc <ipz221_mms@student.ztu.edu.ua>
2024-01-30 14:31:46 +0100

    console app added

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("First number: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Second number: ");
+int y = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Sum = {x + y}");
